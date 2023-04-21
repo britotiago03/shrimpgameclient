16fd858a132dd24dea70a814503575850bf8f457
britotiago03 <69848652+britotiago03@users.noreply.github.com>
2023-04-21 03:44:52 +0200

    v1.6.9 Added gif as background

---
+++ src/main/java/org/example/logic/Timer.java
@@ -0,0 +1,74 @@
+package org.example.logic;
+
+import java.util.List;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+import org.example.ShrimpGameApp;
+
+public class Timer {
+  private int secondsLeft;
+  private ShrimpGameApp shrimpGameApp;
+  private Timeline timeline;
+  private int roundNum;
+
+  public Timer(ShrimpGameApp shrimpGameApp, List<Label> timeLabels) {
+    this.shrimpGameApp = shrimpGameApp;
+    this.secondsLeft = shrimpGameApp.getGame().getSettings().getRoundTime();
+    this.roundNum = shrimpGameApp.getGame().getCurrentRoundNum();
+    this.timeline = new Timeline(new KeyFrame(Duration.seconds(1), e ->
+    {
+      // Update the time left
+      this.secondsLeft--;
+
+      // Update the label with the new time left
+      int minutes = this.secondsLeft / 60;
+      int seconds = this.secondsLeft % 60;
+      String timeLeftStr = String.format("%02d:%02d", minutes, seconds);
+      for (Label timeLeftLbl : timeLabels)
+      {
+        timeLeftLbl.setText(timeLeftStr);
+      }
+
+      // If the time is up, stop the timeline
+      if (this.secondsLeft == 0) {
+        this.timeline.stop();
+        if (shrimpGameApp.getGame().getCurrentRoundNum() == this.roundNum) {
+          try {
+            if (!this.shrimpGameApp.getGame().getPlayers().get(
+                this.shrimpGameApp.getUser().getName()).hasCaughtShrimp()) {
+              int minShrimpKg = this.shrimpGameApp.getGame().getSettings().getMinShrimpPounds();
+              this.shrimpGameApp.getServerConnection().sendCatchShrimpRequest(minShrimpKg);
+              this.shrimpGameApp.getGame().getPlayers().get(this.shrimpGameApp.getUser().getName())
+                                .setShrimpCaught(minShrimpKg);
+              this.shrimpGameApp.getServerConnection().getNextServerPacket();
+            }
+          }
+          catch (RuntimeException exception) {
+            Alert errorDialog = new Alert(Alert.AlertType.ERROR);
+            errorDialog.setTitle("Error");
+            errorDialog.setHeaderText(null);
+            errorDialog.setContentText(exception.getMessage());
+            this.shrimpGameApp.addIconToDialog(errorDialog);
+            errorDialog.showAndWait();
+          }
+        }
+      }
+    }));
+    this.timeline.setCycleCount(Timeline.INDEFINITE);
+  }
+
+  public void start() {
+    this.timeline.play();
+  }
+
+  public void stop() {
+    this.timeline.stop();
+  }
+
+  public int getSecondsLeft() {
+    return secondsLeft;
+  }
+}
