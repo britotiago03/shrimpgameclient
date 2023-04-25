57f73eaa6bc0612b3628f9a769af2d8a7fe25520
Andreas Malmedal <andreas07.malmedal@gmail.com>
2023-04-25 13:20:05 +0200

    Added missing javadoc for GameScreen methods.

--- src/main/java/org/example/userinterface/GameScreen.java
+++ src/main/java/org/example/userinterface/GameScreen.java
@@ -42,10 +42,18 @@ public abstract class GameScreen {
   private static Image OVERVIEW_BACKGROUND;
   public static List<Label> amountOfShrimpCaughtValueLabels = new ArrayList<>();
 
+  /**
+   * Sets the OPTION field.
+   * 
+   * @param option the option to assign to the {@code GameScreen}
+   */
   public static void setOPTION(String option) {
     OPTION = option;
   }
 
+  /**
+   * Initializes the background image for the overview.
+   */
   public static void initOverviewBackgroundImage() {
     OVERVIEW_BACKGROUND = new Image(
         ShrimpGameApp.class.getResource("/images/overview.jpg").toExternalForm());
@@ -152,6 +160,11 @@ public abstract class GameScreen {
     return gameScene;
   }
 
+  /**
+   * Creates the panes for the game screen.
+   * 
+   * @param shrimpGameApp the {@code ShrimpGameApp} object used to get the game information.
+   */
   private static void createPanes(ShrimpGameApp shrimpGameApp) {
     OVERVIEW = GameScreen.getContentPane(shrimpGameApp, "Overview", false);
     OVERVIEW_CAUGHT_SHRIMP = GameScreen.getContentPane(shrimpGameApp, "Overview", true);
@@ -160,6 +173,14 @@ public abstract class GameScreen {
     CHAT = GameScreen.getContentPane(shrimpGameApp, "Chat", false);
   }
 
+  /**
+   * Gets a specified content pane.
+   * 
+   * @param shrimpGameApp the {@code ShrimpGameApp} object used to get the game information.
+   * @param option the name of the content pane.
+   * @param hasCaughtShrimp a {@code boolean} value determining whether or not the player has caught shrimp.
+   * @return
+   */
   private static VBox getContentPane(ShrimpGameApp shrimpGameApp, String option,
                                      boolean hasCaughtShrimp) {
     VBox content = new VBox();
