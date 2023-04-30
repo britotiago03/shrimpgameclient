df99cc9db3fa5b4527036abba58fd012ee5ee4b5
britotiago03 <69848652+britotiago03@users.noreply.github.com>
2023-04-30 02:40:18 +0200

    v1.7.7 Fixed version of pom.xml file

--- src/main/java/org/example/ShrimpGameApp.java
+++ src/main/java/org/example/ShrimpGameApp.java
@@ -69,7 +69,7 @@ import org.example.ui.view.ShrimpPriceCalculationScreen;
  */
 public class ShrimpGameApp extends Application {
   private Stage primaryStage;
-  public static final String VERSION = "1.7.6";
+  public static final String VERSION = "1.7.7";
   private static final String HOSTNAME = "35.228.211.136";
   private static final int PORT = 8080;
   private Scene mainScreen; 
